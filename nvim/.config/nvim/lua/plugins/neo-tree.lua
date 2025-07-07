7289701432a4669ffa8bad55fcda18d2da0108ba
Jerome Chong <jeromechong229@gmail.com>
2025-07-07 20:28:50 +0100

    neotree show hidden files

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -15,6 +15,11 @@ return {
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
       follow_current_file = {
         enabled = true,
       },
